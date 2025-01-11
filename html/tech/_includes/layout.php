d288b790c87fa53893043808b544a935d9c81169
Rael B. Riolino <rael82@gmail.com>
2025-01-11 19:43:58 -0300

    feat: finished post page

--- html/tech/_includes/layout.php
+++ html/tech/_includes/layout.php
@@ -34,7 +34,8 @@
 		<nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="logo">
               <a role="brand" href="<?php echo $GLOBALS['root']; ?>/">
-                <span>robotshack</span>
+                <img src="../_assets/svg/logo.svg" />
+                <span><b>robot</b>shack</span>
               </a>
             </div>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
--- html/tech/posts/post_view.php
+++ html/tech/posts/post_view.php
@@ -8,194 +8,27 @@
 </head>
 <body>
 <?php get_body_header(); ?>
-<?php get_carousel_header(); ?>
   <!-- banner section end -->
   <!-- about section start -->
   <div class="about_section layout_padding">
     <div class="container">
       <div class="row">
-        <div class="col-md-6">
-          <div class="image_2"><img src="images/img-2.png"></div>
-        </div>
-        <div class="col-md-6">
-          <h1 class="about_text">ABOUT</h1>
-          <p class="lorem_text">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable</p>
-          <div class="shop_bt_2"><a href="#">Shop Now</a></div>
-        </div>
+        <article class="post col-md-6">
+          <header>
+            <h1 class="post_h1">Researchers open source Sky-T1, a ‘reasoning’ AI model that can be trained for less than $450</h1>
+          </header>
+          <main>
+            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable</p>
+            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable</p>
+            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable</p>
+            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable</p>
+          </main>
+        </article>
       </div>
     </div>
   </div>
   <!-- about section end -->
-  <!-- product section start -->
-  <div class="product_section layout_padding">
-    <div class="container">
-      <div class="product_text">Our <span style="color: #5ca0e9;">products</span></div>
-      <p class="long_text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem</p>
-      <div class="product_section_2">
-        <div class="row">
-          <div class="col-md-6">
-            <div class="image_2"><img src="images/img-3.png"></div>
-            <div class="price_text">Price $ <span style="color: #3a3a38;">200</span></div>
-            <h1 class="game_text">Video Game</h1>
-            <p class="long_text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem</p>
-          </div>
-          <div class="col-md-6">
-            <div class="image_2"><img src="images/img-3.png"></div>
-            <div class="price_text">Price $ <span style="color: #3a3a38;">300</span></div>
-            <h1 class="game_text">Video Game</h1>
-            <p class="long_text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem</p>
-          </div>
-        </div>
-      </div>
-      <div class="see_main">
-        <div class="see_bt"><a href="#">See More</a></div>
-      </div>
-    </div>
-  </div>
-  <!-- product section end -->
-  <!-- video section start -->
-  <div class="video_section layout_padding">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6">
-          <h1 class="video_text_2">3d video game</h1>
-          <h1 class="controller_text_2">remoto control</h1>
-          <p class="banner_text_2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud</p>
-          <div class="shop_bt"><a href="#">Shop Now</a></div>
-        </div>
-        <div class="col-md-6">
-          <div class="image_4"><img src="images/img-4.png"></div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- video section end -->
-  <!-- testimonial section start -->
-  <div class="testimonial_section layout_padding">
-    <div class="container">
-      <h1 class="testimonial_text">Testimonial</h1>
-      <p class="ipsum_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud</p>
-      <div class="testimonial_section_2">
-        <div class="box_main">
-          <div class="quote_icon"><img src="images/quote-icon.png"></div>
-          <p class="dolor_text"> dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum </p>
-          <div class="client_main">
-            <div class="client_left">
-              <div class="images_5"><img src="images/img-5.png"></div>
-            </div>
-            <div class="client_right">
-              <h1 class="sandy_text">Sandy Delex</h1>
-              <p class="sandy_text_1">Reprehenderit</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="see_main">
-        <div class="see_bt"><a href="#">See More</a></div>
-      </div>
-    </div>
-  </div>
-  <!-- testimonial section end -->
-  <!-- contact section start -->
-  <div class="contact_section layout_padding">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6">
-          <div class="email_box">
-                    <div class="input_main">
-                       <div class="container">
-                          <form action="/action_page.php">
-                            <div class="form-group">
-                              <input type="text" class="email-bt" placeholder="Name" name="Name">
-                            </div>
-                            <div class="form-group">
-                              <input type="text" class="email-bt" placeholder="Email" name="Name">
-                            </div>
-                            <div class="form-group">
-                              <input type="text" class="email-bt" placeholder="Phone Numbar" name="Email">
-                            </div>
-                            <div class="form-group">
-                                <textarea class="massage-bt" placeholder="Massage" rows="5" id="comment" name="Massage"></textarea>
-                            </div>
-                          </form>   
-                       </div> 
-                        <div class="main_bt"><a href="#">SEND</a></div>                  
-                    </div>
-                 </div>
-        </div>
-        <div class="col-md-6">
-          <div class="image_6"><img src="images/img-6.png"></div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- contact section end -->
-  <!-- footer section start -->
-  <div class="section_footer ">
-      <div class="container"> 
-          <div class="footer_section_2">
-            <div class="row">
-                <div class="col-sm-6 col-md-6 col-lg-3">
-                  <h2 class="account_text">About Us</h2>
-                  <p class="ipsum_text_2">dolor sit amet, consectetur magna aliqua. Ut enim ad minim veniam, quisdotempor incididunt r</p>
-                </div>
-                <div class="col-sm-6 col-md-6 col-lg-3">
-                  <h2 class="account_text">Useful Link</h2>
-                  <div class="useful_link">
-                    <ul>
-                      <li><a href="#">Video games</a></li>
-                      <li><a href="#">Remote control</a></li>
-                      <li><a href="#">3d controller</a></li>
-                    </ul>
-                  </div>
-                </div>
-                <div class="col-sm-6 col-md-6 col-lg-3">
-                <h2 class="account_text">Contact Us</h2>
-                <p class="ipsum_text_2">dolor sit amet, consectetur magna aliqua. quisdotempor incididunt ut e </p>
-                </div>
-          <div class="col-sm-6 col-md-6 col-lg-3">
-            <h2 class="account_text">Newsletter</h2>
-            <input type="" class="email_text" placeholder="Enter Your Email" name="Enter Your Email">
-            <button class="subscribr_bt">SUBSCRIBE</button>
-          </div>
-        </div>
-      </div>
-      <div class="social_icon">
-        <ul>
-          <li><a href="#"><img src="images/fb-icon.png"></a></li>
-          <li><a href="#"><img src="images/twitter-icon.png"></a></li>
-          <li><a href="#"><img src="images/linkdin-icon.png"></a></li>
-          <li><a href="#"><img src="images/instagram-icon.png"></a></li>
-        </ul>
-      </div>
-    </div>
-  </div>
-  <!-- footer section end -->
-  <!-- copyright section start -->
-  <div class="copyright_section">
-    <div class="container">
-      <p class="copyright_text">Copyright 2020 All Right Reserved By <a href="https://html.design/"> Free html Templates</p>
-    </div>
-  </div>
-  <!-- copyright section end -->
-  <!-- Javascript files-->
-    <script src="js/jquery.min.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.bundle.min.js"></script>
-    <script src="js/jquery-3.0.0.min.js"></script>
-    <script src="js/plugin.js"></script>
-    <!-- sidebar -->
-    <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
-    <script src="js/custom.js"></script>
-    <!-- javascript --> 
-    <script src="js/owl.carousel.js"></script>
-    <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
-    <script>
-    $(document).ready(function(){
-    $(".fancybox").fancybox({
-        openEffect: "none",
-        closeEffect: "none"
-        });
-    </script>
+  <?php get_body_footer(); ?>
+  <?php get_js_scripts(); ?>
 </body>
 </html>
